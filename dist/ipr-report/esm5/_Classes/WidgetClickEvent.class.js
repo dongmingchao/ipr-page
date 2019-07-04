4703aafd0167e3eec8c58de9610f6becebd3e049
dongmingchao <1657883517@qq.com>
2019-07-04 12:07:55 +0800

    2019年07月 4日 12:07:55

--- dist/ipr-report/esm5/_Classes/WidgetClickEvent.class.js
+++
@@ -1,7 +0,0 @@
-var WidgetClickEvent = /** @class */ (function () {
-    function WidgetClickEvent() {
-    }
-    return WidgetClickEvent;
-}());
-export { WidgetClickEvent };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiV2lkZ2V0Q2xpY2tFdmVudC5jbGFzcy5qcyIsInNvdXJjZVJvb3QiOiJuZzovL2lwci1yZXBvcnQvIiwic291cmNlcyI6WyJfQ2xhc3Nlcy9XaWRnZXRDbGlja0V2ZW50LmNsYXNzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUVBO0lBQUE7SUFJQSxDQUFDO0lBQUQsdUJBQUM7QUFBRCxDQUFDLEFBSkQsSUFJQyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7Q2F0YWxvZ30gZnJvbSAnLi9DYXRhbG9nLmNsYXNzJztcclxuXHJcbmV4cG9ydCBjbGFzcyBXaWRnZXRDbGlja0V2ZW50IHtcclxuICAgIHNvdXJjZV9ldmVudDogYW55O1xyXG4gICAgY2F0YWxvZ3M6IENhdGFsb2dbXTtcclxuICAgIHdpZGdldF9uYW1lOiBzdHJpbmc7XHJcbn1cclxuIl19
