119f764a6e12b3ba38fea0a93398e0b8eb491bd4
dongmingchao <1657883517@qq.com>
2019-07-05 10:56:31 +0800

    2019年07月 5日 10:56:30

---
+++ esm2015/components/page/paragraph-placeholder/paragraph-placeholder.component.js
@@ -0,0 +1,13 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+let ParagraphPlaceholderComponent = class ParagraphPlaceholderComponent {
+};
+ParagraphPlaceholderComponent = tslib_1.__decorate([
+    Component({
+        selector: 'ipr-paragraph-placeholder',
+        template: "<div class=\"title-placeholder\"></div>\r\n<div class=\"text-placeholder\"></div>\r\n<div class=\"link-placeholder\"></div>\r\n",
+        styles: [":host .title-placeholder{height:1.8rem;margin-bottom:.5rem;width:20%}:host .text-placeholder{height:4rem;margin-bottom:1rem}:host .link-placeholder{height:1.25rem;width:5rem}:host [class$=placeholder]{background:#e4e9f2}"]
+    })
+], ParagraphPlaceholderComponent);
+export { ParagraphPlaceholderComponent };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGFyYWdyYXBoLXBsYWNlaG9sZGVyLmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiJuZzovL2lwci1yZXBvcnQvIiwic291cmNlcyI6WyJjb21wb25lbnRzL3BhZ2UvcGFyYWdyYXBoLXBsYWNlaG9sZGVyL3BhcmFncmFwaC1wbGFjZWhvbGRlci5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE9BQU8sRUFBRSxTQUFTLEVBQWUsTUFBTSxlQUFlLENBQUM7QUFPdkQsSUFBYSw2QkFBNkIsR0FBMUMsTUFBYSw2QkFBNkI7Q0FFekMsQ0FBQTtBQUZZLDZCQUE2QjtJQUx6QyxTQUFTLENBQUM7UUFDVCxRQUFRLEVBQUUsMkJBQTJCO1FBQ3JDLDJJQUFtRDs7S0FFcEQsQ0FBQztHQUNXLDZCQUE2QixDQUV6QztTQUZZLDZCQUE2QiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCwgSG9zdEJpbmRpbmcgfSBmcm9tICdAYW5ndWxhci9jb3JlJztcclxuXHJcbkBDb21wb25lbnQoe1xyXG4gIHNlbGVjdG9yOiAnaXByLXBhcmFncmFwaC1wbGFjZWhvbGRlcicsXHJcbiAgdGVtcGxhdGVVcmw6ICdwYXJhZ3JhcGgtcGxhY2Vob2xkZXIuY29tcG9uZW50Lmh0bWwnLFxyXG4gIHN0eWxlVXJsczogWydwYXJhZ3JhcGgtcGxhY2Vob2xkZXIuY29tcG9uZW50LnN0eWwnXSxcclxufSlcclxuZXhwb3J0IGNsYXNzIFBhcmFncmFwaFBsYWNlaG9sZGVyQ29tcG9uZW50IHtcclxuXHJcbn1cclxuIl19
---
+++ esm5/components/page/paragraph-placeholder/paragraph-placeholder.component.js
@@ -0,0 +1,16 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var ParagraphPlaceholderComponent = /** @class */ (function () {
+    function ParagraphPlaceholderComponent() {
+    }
+    ParagraphPlaceholderComponent = tslib_1.__decorate([
+        Component({
+            selector: 'ipr-paragraph-placeholder',
+            template: "<div class=\"title-placeholder\"></div>\r\n<div class=\"text-placeholder\"></div>\r\n<div class=\"link-placeholder\"></div>\r\n",
+            styles: [":host .title-placeholder{height:1.8rem;margin-bottom:.5rem;width:20%}:host .text-placeholder{height:4rem;margin-bottom:1rem}:host .link-placeholder{height:1.25rem;width:5rem}:host [class$=placeholder]{background:#e4e9f2}"]
+        })
+    ], ParagraphPlaceholderComponent);
+    return ParagraphPlaceholderComponent;
+}());
+export { ParagraphPlaceholderComponent };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGFyYWdyYXBoLXBsYWNlaG9sZGVyLmNvbXBvbmVudC5qcyIsInNvdXJjZVJvb3QiOiJuZzovL2lwci1yZXBvcnQvIiwic291cmNlcyI6WyJjb21wb25lbnRzL3BhZ2UvcGFyYWdyYXBoLXBsYWNlaG9sZGVyL3BhcmFncmFwaC1wbGFjZWhvbGRlci5jb21wb25lbnQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE9BQU8sRUFBRSxTQUFTLEVBQWUsTUFBTSxlQUFlLENBQUM7QUFPdkQ7SUFBQTtJQUVBLENBQUM7SUFGWSw2QkFBNkI7UUFMekMsU0FBUyxDQUFDO1lBQ1QsUUFBUSxFQUFFLDJCQUEyQjtZQUNyQywySUFBbUQ7O1NBRXBELENBQUM7T0FDVyw2QkFBNkIsQ0FFekM7SUFBRCxvQ0FBQztDQUFBLEFBRkQsSUFFQztTQUZZLDZCQUE2QiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCwgSG9zdEJpbmRpbmcgfSBmcm9tICdAYW5ndWxhci9jb3JlJztcclxuXHJcbkBDb21wb25lbnQoe1xyXG4gIHNlbGVjdG9yOiAnaXByLXBhcmFncmFwaC1wbGFjZWhvbGRlcicsXHJcbiAgdGVtcGxhdGVVcmw6ICdwYXJhZ3JhcGgtcGxhY2Vob2xkZXIuY29tcG9uZW50Lmh0bWwnLFxyXG4gIHN0eWxlVXJsczogWydwYXJhZ3JhcGgtcGxhY2Vob2xkZXIuY29tcG9uZW50LnN0eWwnXSxcclxufSlcclxuZXhwb3J0IGNsYXNzIFBhcmFncmFwaFBsYWNlaG9sZGVyQ29tcG9uZW50IHtcclxuXHJcbn1cclxuIl19
